40f4f8466871fe7b39183827ea21aec626a18131
sammeowwww <lausamuel655@gmail.com>
2026-08-18 14:30:22 +0800

    Add indentation

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -2,29 +2,29 @@ import java.util.Scanner;
 
 public class Bob {
     private static void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("        ___________________________________________________________________");
     }
 
     private static void printBanner() {
-        System.out.println("  ____        _     ");
-        System.out.println(" |  _ \\      | |    ");
-        System.out.println(" | |_) | ___ | |__  ");
-        System.out.println(" |  _ < / _ \\| '_ \\ ");
-        System.out.println(" | |_) | (_) | |_) |");
-        System.out.println(" |____/ \\___/|_.__/ ");
+        System.out.println("        ____        _     ");
+        System.out.println("        |  _ \\      | |    ");
+        System.out.println("        | |_) | ___ | |__  ");
+        System.out.println("        |  _ < / _ \\| '_ \\ ");
+        System.out.println("        | |_) | (_) | |_) |");
+        System.out.println("        |____/ \\___/|_.__/ ");
     }
 
     private static void printWelcome() {
         printLine();
         printBanner();
         printLine();
-        System.out.println("Hey!! I'm Mr Bob, your friendly and personalised assistant.");
-        System.out.println("Anyways, what can i do for you today?");
+        System.out.println("        Hey!! I'm Mr Bob, your friendly and personalised assistant.");
+        System.out.println("        Anyways, what can i do for you today?");
         printLine();
     }
 
     private static void printBye() {
-        System.out.println("Bye! See you later alligator!");
+        System.out.println("        Bye! See you later alligator!");
     }
 
     public static void main(String[] args) {
@@ -38,7 +38,7 @@ public class Bob {
             if  (command.equals("bye")) {
                 break;
             } else {
-                System.out.println(command); // reads a command from user
+                System.out.println("        " + command); // reads a command from user
                 printLine();
             }
         }
