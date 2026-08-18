81152eef68abc6293fc99a78da036fed435e07e5
sammeowwww <lausamuel655@gmail.com>
2026-08-18 12:21:32 +0800

    Update greeting and goodbye

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,5 +1,20 @@
 public class Bob {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("  ____        _     ");
+        System.out.println(" |  _ \\      | |    ");
+        System.out.println(" | |_) | ___ | |__  ");
+        System.out.println(" |  _ < / _ \\| '_ \\ ");
+        System.out.println(" | |_) | (_) | |_) |");
+        System.out.println(" |____/ \\___/|_.__/ ");
+        System.out.println("____________________________________________________________\n");
 
+        System.out.println("Hey!! I'm Mr Bob, your friendly and personalised assistant.");
+        System.out.println("Anyways, what can i do for you today?\n");
+
+
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Bye! See you later alligator!\n");
+        System.out.println("____________________________________________________________");
     }
 }
