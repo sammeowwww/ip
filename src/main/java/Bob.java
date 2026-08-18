ce265a8ba804182f4d483c8b21e949afc67a0229
sammeowwww <lausamuel655@gmail.com>
2026-08-18 14:25:34 +0800

    Echo commands

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,20 +1,50 @@
+import java.util.Scanner;
+
 public class Bob {
-    public static void main(String[] args) {
+    private static void printLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    private static void printBanner() {
         System.out.println("  ____        _     ");
         System.out.println(" |  _ \\      | |    ");
         System.out.println(" | |_) | ___ | |__  ");
         System.out.println(" |  _ < / _ \\| '_ \\ ");
         System.out.println(" | |_) | (_) | |_) |");
         System.out.println(" |____/ \\___/|_.__/ ");
-        System.out.println("____________________________________________________________\n");
+    }
 
+    private static void printWelcome() {
+        printLine();
+        printBanner();
+        printLine();
         System.out.println("Hey!! I'm Mr Bob, your friendly and personalised assistant.");
-        System.out.println("Anyways, what can i do for you today?\n");
+        System.out.println("Anyways, what can i do for you today?");
+        printLine();
+    }
 
+    private static void printBye() {
+        System.out.println("Bye! See you later alligator!");
+    }
 
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Bye! See you later alligator!\n");
-        System.out.println("____________________________________________________________");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //welcome statement
+        printWelcome();
+
+        while (true) {
+            String command =  input.nextLine();
+            if  (command.equals("bye")) {
+                break;
+            } else {
+                System.out.println(command); // reads a command from user
+                printLine();
+            }
+        }
+
+        input.close();
+        printBye();
+        printLine();
     }
 }
